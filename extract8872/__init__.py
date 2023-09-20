5d98fad64790c8e82294236dd923d0b570b9b656
Andy Friedman <afriedman412@gmail.com>
2023-09-20 16:46:10 -0400

    first commit

---
+++ extract8872/__init__.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, render_template, jsonify, session, send_file
+from .helpers import extract_one_file, create_files
+import io
+import pdfplumber
+import zipfile
+import tempfile
+import os
+
+app = Flask(__name__)
+app.secret_key = "xxxxxx"
+
+@app.route('/')
+def home():
+    return render_template('index.html', content_page="drop.html")
+
+@app.route('/upload', methods=['POST'])
+def upload_pdf():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'})
+
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'})
+
+    pdf_data = io.BytesIO(file.read())
+    p = pdfplumber.open(pdf_data)
+    extracted_data = extract_one_file(p)
+    file_name = file.filename.replace(".pdf", "")
+    session['arc_path'] = create_files(extracted_data, file_name)
+    
+    return jsonify({'message': 'File uploaded successfully'})
+
+@app.route('/download')
+def download():
+    return send_file(session['arc_path'])
---
+++ extract8872/helpers.py
@@ -0,0 +1,77 @@
+import pdfplumber
+import pandas as pd
+from typing import Literal
+import re
+import tempfile
+import os
+import zipfile
+
+def extract_one_page(page):
+    page_text = page.extract_text()
+    if page_text.startswith("Contributor"):
+        entries = [e for e in page.extract_text().split(
+            "Contributor'sname,mailingaddressandZIPcode Nameofcontributor'semployer") if e]
+
+        data = []
+        for e_ in entries:
+            name = e_.split("\n")[1].split(" ")[0]
+            if re.search(r"\$\s?\d+(?:[\d\/]+)", e_.split("\n")[5]):
+                cash = re.search(r"\$\s?\d+(?:[\d\/]+)", e_.split("\n")[5])[0].replace(" ", "")
+            else:
+                cash = "$0"
+            data.append((name, cash))
+        return data, 'a'
+    
+    elif page_text.startswith("Recipient'sname"):
+        entries = [
+            e for e in page.extract_text().split(
+        "Recipient'sname,mailingaddressandZIPcode Nameofrecipient'semployer AmountofExpenditure") if e]
+
+        data = []
+        for e_ in entries:
+            name_cash = e_.split("\n")[1].replace("$ ", "$").split(" ")
+            name_cash = [name_cash[0], name_cash[-1], e_.split("\n")[5]]
+            data.append(name_cash)
+        return data, 'b'
+    else:
+        return None, False
+    
+def extract_one_file(
+    p: pdfplumber.pdf.PDF,
+    track_pages: bool=False
+):
+    data_out = {
+        "a": [],
+        "b": []
+    }
+    for n, page in enumerate(p.pages):
+        if track_pages:
+            print(n)
+        data, schedule = extract_one_page(page)
+        if data:
+            data_out[schedule]+=(data)
+    return data_out
+
+def create_files(data_out, file_name):
+    temp_dir = tempfile.mkdtemp()
+    file_paths = []
+    for schedule, data in data_out.items():
+        df = make_df(data, schedule)
+        file_path = os.path.join(temp_dir, f"{file_name}_{schedule}.csv")
+        df.to_csv(os.path.join(temp_dir, file_path))
+        file_paths.append(file_path)
+
+    arc_path = os.path.join(temp_dir, file_name+".zip")
+    with zipfile.ZipFile(arc_path, "w") as zip:
+        for file_path in file_paths:
+            zip.write(file_path, arcname=arc_path)
+    return arc_path
+
+def make_df(
+        data: list,
+        schedule: Literal['a', 'b']
+        ):
+    columns = ["Contributor", "Amount"] if schedule=='a' else ['Recipient', 'Amount', 'Purpose']
+    df = pd.DataFrame(data, columns=columns)
+    return df
+    
